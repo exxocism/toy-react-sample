410bbb0144575a5625d5e9d20bc8da5ee1a6b74d
Charlie_mac <tchoi.tab@gmail.com>
2021-09-15 16:52:42 +0900

    App.js Component Applied

--- src/App.js
+++ src/App.js
@@ -1,36 +1,102 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route, Link } from 'react-router-dom';
 
-function App() {
-  
-  let [testval, testval_change] = useState("'Watch your language'");
+const Navbar = () => {
+
+  return (
+    <div className="nav-top">
+        <div className="nav-top-name">
+          <i className="fab fa-react"></i>
+          <span>EAX Enterprises GmbH</span>  
+        </div>
+        <NbarList />
+    </div>
+  );
+
+};
+
+const NbarList = () => {
+
+  return (
+    <div className="nav-top-list">
+      <span><Link to="/">Home</Link></span>
+      <span><Link to="/about">Mypage</Link></span>
+      <span><Link to="/dashboard">Dashboard</Link></span>
+    </div>
+  );
+
+}
+
+const SimpleList = () => {
+
   let [sucktext, assign_sucktext] = useState ('Flip');
   let suckcount = true;
 
   return (
-    <div className="App">
-      <div className="nav-top">
-        <div>EAX Enterprises GmbH</div>  
-      </div>
-      <div className="list">
+    <div className="list">
         <h3>Click this image to check data binding <img src={logo} width={30} alt="Logo" onClick={ ()=>{ suckcount = !suckcount; assign_sucktext((suckcount)? 'Flip':'Flop'); }}/> {sucktext}</h3>
         <p class="semi-text">test man</p>
         <hr/>
-      </div>
-      <div className="maintext">
-        <ul>
-          <li>This is a sample message</li>
-          <p className="talkbox">
-          javscript:alert ({testval});
-          </p>
-          <br />
-          <li>And also a sample message</li>
-          <li>Triple Check!</li>
-          <li>And Four!</li>
-        </ul>
-        
-      </div>  
+    </div>
+  );
+
+};
+
+const Maintext = () => {
+
+  let [testval, testval_change] = useState("'Watch your language'");
+
+  return (
+    <div className="maintext">
+      <ul>
+        <li>This is a sample message</li>
+        <p className="talkbox">
+        javscript:alert ({testval});
+        </p>
+        <br />
+        <li>And also a sample message</li>
+        <li>Triple Check!</li>
+        <li>And Four!</li>
+      </ul>
+      
+    </div>
+  );
+
+}
+
+const About = () => {
+
+  return (
+    <div>I am about</div>
+  );
+
+};
+
+const Dashboard = () => {
+
+  return (
+    <div>I am Dashboard</div>
+  );
+
+};
+
+function App() {
+  
+  return (
+    <div className="App">
+      <Navbar />
+      <Route exact path="/">
+        <SimpleList />
+        <Maintext />
+      </Route>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route path="/dashboard">
+        <Dashboard />
+      </Route>
     </div>
   );
 }
